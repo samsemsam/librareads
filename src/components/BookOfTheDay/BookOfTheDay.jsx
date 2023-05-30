a8bdd05d9d9d5e78526f04305cc0136c76761a61
samsemsam <132723953+samsemsam@users.noreply.github.com>
2023-05-30 23:16:38 +0700

    LibraReads

--- src/components/BookOfTheDay/BookOfTheDay.jsx
+++ src/components/BookOfTheDay/BookOfTheDay.jsx
@@ -5,77 +5,75 @@ import './BookOfTheDay.css';
 import { Link } from 'react-router-dom';
 
 const BookOfTheDay = () => {
-    const [bookOfTheDay, setBookOfTheDay] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const [bookOfTheDay, setBookOfTheDay] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setLoading(true);
-        async function fetchBookOfTheDay() {
-            try {
-                const response = await fetch("http://openlibrary.org/search.json?title=book");
-                const data = await response.json();
+  useEffect(() => {
+    setLoading(true);
+    async function fetchBookOfTheDay() {
+      try {
+        const response = await fetch("http://openlibrary.org/search.json?title=book");
+        const data = await response.json();
 
-                if (data.docs) {
-                    const bookList = data.docs.map((book) => ({
-                        id: book.key.replace(/^\/works\//, ""),
-                        title: book.title,
-                        author: book.author_name || [],
-                        cover_id: book.cover_i,
-                        edition_count: book.edition_count,
-                        first_publish_year: book.first_publish_year
-                    }));
+        if (data.docs) {
+          const bookList = data.docs.map((book) => ({
+            id: book.key.replace(/^\/works\//, ""),
+            title: book.title,
+            author: book.author_name || [],
+            cover_id: book.cover_i,
+            edition_count: book.edition_count,
+            first_publish_year: book.first_publish_year
+          }));
 
-                    const randomIndex = Math.floor(Math.random() * bookList.length);
-                    const selectedBook = bookList[randomIndex];
+          const randomIndex = Math.floor(Math.random() * bookList.length);
+          const selectedBook = bookList[randomIndex];
 
-                    if (selectedBook.cover_id) {
-                        selectedBook.cover_img = `https://covers.openlibrary.org/b/id/${selectedBook.cover_id}-L.jpg`;
-                    } else {
-                        selectedBook.cover_img = coverImg;
-                    }
+          if (selectedBook.cover_id) {
+            selectedBook.cover_img = `https://covers.openlibrary.org/b/id/${selectedBook.cover_id}-L.jpg`;
+          } else {
+            selectedBook.cover_img = coverImg;
+          }
 
-                    setBookOfTheDay(selectedBook);
-                }
-
-                setLoading(false);
-            } catch (error) {
-                console.log(error);
-                setLoading(false);
-            }
+          setBookOfTheDay(selectedBook);
         }
 
-        fetchBookOfTheDay();
-    }, []);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
 
-    const handleLinkClick = () => {
-        navigate(`/book/${bookOfTheDay.id}`);
-    };
+    fetchBookOfTheDay();
+  }, []);
 
-    return (
-        <div className="book-of-the-day">
-            <h2>Book of the Day</h2>
-            {bookOfTheDay ? (
-                <div className="book-container">
-                    <Link to={`/book/${bookOfTheDay.id}`} onClick={handleLinkClick}>
-                        <img src={bookOfTheDay.cover_img} alt="cover" />
-                    </Link>
-                    <div className="book-info">
-                        <Link to={`/book/${bookOfTheDay.id}`} onClick={handleLinkClick}>
-                            <h3>{bookOfTheDay.title}</h3>
-                        </Link>
-                        <p>Author: {bookOfTheDay.author.join(", ")}</p>
-                        <p>Editions: {bookOfTheDay.edition_count}</p>
-                        <p>First Publish Year: {bookOfTheDay.first_publish_year}</p>
-                    </div>
-                </div>
+  const handleLinkClick = () => {
+    navigate(`/book/${bookOfTheDay.id}`);
+  };
 
-            ) : (
-                <p>Loading...</p>
-            )}
+  return (
+    <div className="book-of-the-day">
+      <h2>Book of the Day</h2>
+      {bookOfTheDay ? (
+        <div className="book-container">
+          <Link to={`/book/${bookOfTheDay.id}`} onClick={handleLinkClick}>
+            <img src={bookOfTheDay.cover_img} alt="cover" />
+          </Link>
+          <div className="book-info">
+            <Link to={`/book/${bookOfTheDay.id}`} onClick={handleLinkClick}>
+              <h3>{bookOfTheDay.title}</h3>
+            </Link>
+            <p>Author: {bookOfTheDay.author.join(", ")}</p>
+            <p>Editions: {bookOfTheDay.edition_count}</p>
+            <p>First Publish Year: {bookOfTheDay.first_publish_year}</p>
+          </div>
         </div>
-
-    );
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default BookOfTheDay;
