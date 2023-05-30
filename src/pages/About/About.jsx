59e8ca2c74cd0e9f83d399bc55123b4b1027af0c
samsemsam <132723953+samsemsam@users.noreply.github.com>
2023-05-30 22:39:14 +0700

    LibraReads

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,20 +4,26 @@ import aboutWira from '../../images/wira.JPG';
 import aboutRiffa from '../../images/riffa.JPG';
 import aboutAthar from '../../images/athar.JPG';
 import aboutSam from '../../images/sam.JPG';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faSteam } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const About = () => {
   return (
     <section className='about'>
       <div className='about-container'>
-<div className='about-text'>
+        <div className='about-text'>
           <h2 className='fs-26 ls-1'>About Us</h2>
-          <p className='fs-17'>LibraReads adalah sebuah website yang menyediakan koleksi buku yang dapat Anda baca secara online. 
-          Temukan berbagai judul menarik dari berbagai genre di perpustakaan digital kami. 
-          Nikmati pengalaman membaca yang menyenangkan dengan fitur unggulan kami, 
-          yaitu kemampuan untuk melihat preview yang jelas dari setiap buku sebelum Anda memutuskan untuk membacanya secara lengkap. 
-          Kami juga menawarkan fitur menarik lainnya, yaitu pilihan untuk memilih buku secara acak, 
-          memberikan kejutan menyenangkan setiap kali Anda ingin mengeksplorasi buku baru. 
-          Bergabunglah dengan kami dan jelajahi dunia pengetahuan dan hiburan melalui buku-buku digital kami yang menginspirasi.</p>
+          <p className='fs-17'>LibraReads adalah sebuah website yang menyediakan koleksi buku yang dapat Anda baca secara online.
+            Temukan berbagai judul menarik dari berbagai genre di perpustakaan digital kami.
+            Nikmati pengalaman membaca yang menyenangkan dengan fitur unggulan kami,
+            yaitu kemampuan untuk melihat preview yang jelas dari setiap buku sebelum Anda memutuskan untuk membacanya secara lengkap.
+            Kami juga menawarkan fitur menarik lainnya, yaitu pilihan untuk memilih buku secara acak,
+            memberikan kejutan menyenangkan setiap kali Anda ingin mengeksplorasi buku baru.
+            Bergabunglah dengan kami dan jelajahi dunia pengetahuan dan hiburan melalui buku-buku digital kami yang menginspirasi.</p>
           <br /><br />
           <h2 className='about-title fs-26 ls-1'>Our Team</h2>
         </div>
@@ -32,13 +38,13 @@ const About = () => {
             </div>
             <div className='about-sci'>
               <a href='https://github.com/JohnJonnzz'>
-                <i className='fa fa-github'></i>
+                <FontAwesomeIcon icon={faGithub} />
               </a>
               <a href='https://www.instagram.com/wgi_361/'>
-                <i className='fa fa-instagram'></i>
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
               <a href='https://steamcommunity.com/profiles/76561198382830245'>
-                <i className='fa fa-steam'></i>
+                <FontAwesomeIcon icon={faSteam} />
               </a>
             </div>
           </div>
@@ -54,13 +60,13 @@ const About = () => {
             </div>
             <div className='about-sci'>
               <a href='https://github.com/reeves63'>
-                <i className='fa fa-github'></i>
+                <FontAwesomeIcon icon={faGithub} />
               </a>
               <a href='https://www.instagram.com/rf.ptrrr/'>
-                <i className='fa fa-instagram'></i>
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
               <a href='https://steamcommunity.com/profiles/76561198434537025/'>
-                <i className='fa fa-steam'></i>
+                <FontAwesomeIcon icon={faSteam} />
               </a>
             </div>
           </div>
@@ -76,13 +82,13 @@ const About = () => {
             </div>
             <div className='about-sci'>
               <a href='https://github.com/StillFrost75'>
-                <i className='fa fa-github'></i>
+                <FontAwesomeIcon icon={faGithub} />
               </a>
               <a href='https://www.instagram.com/raditya_athar75/'>
-                <i className='fa fa-instagram'></i>
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
               <a href='https://steamcommunity.com/id/kobayakawa75'>
-                <i className='fa fa-steam'></i>
+                <FontAwesomeIcon icon={faSteam} />
               </a>
             </div>
           </div>
@@ -98,13 +104,13 @@ const About = () => {
             </div>
             <div className='about-sci'>
               <a href='https://github.com/samsemsam'>
-                <i className='fa fa-github'></i>
+                <FontAwesomeIcon icon={faGithub} />
               </a>
               <a href='https://www.instagram.com/samtss_/'>
-                <i className='fa fa-instagram'></i>
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
               <a href='https://steamcommunity.com/id/Kaganeka'>
-                <i className='fa fa-steam'></i>
+                <FontAwesomeIcon icon={faSteam} />
               </a>
             </div>
           </div>
